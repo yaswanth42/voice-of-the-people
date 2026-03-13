1fcf9646d1ada4c085604997ab6cafd7aefe1881
yaswanthreddy34 <yaswanth.dandu@izadatatech.se>
2026-03-13 22:48:59 +0530

    Implemented JWT login authentication

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -24,3 +24,31 @@ res.json({message:"User Registered Successfully"})
 })
 
 module.exports = router
+
+const jwt = require("jsonwebtoken")
+
+router.post("/login", async (req,res)=>{
+
+const {email,password} = req.body
+
+const user = await User.findOne({email})
+
+if(!user){
+return res.json({message:"User not found"})
+}
+
+const isMatch = await bcrypt.compare(password,user.password)
+
+if(!isMatch){
+return res.json({message:"Invalid password"})
+}
+
+const token = jwt.sign(
+{userId:user._id},
+"secretkey",
+{expiresIn:"1d"}
+)
+
+res.json({token})
+
+})
